07c464987d7fa27b023ce6e7d8a3954a08111568
AASonar <bryan@acadarena.com>
2022-02-11 15:50:43 +0800

    fixed mapping issue

--- components/ui/nftCard.tsx
+++ components/ui/nftCard.tsx
@@ -1,59 +1,49 @@
+import { NFTCardDetails } from "../types/nftCardType";
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, Grid } from "@mui/material";
-import FetchNFTs from "../moralisAPI";
-import { useEffect, useState, useContext } from "react";
-import { NFTCardDetails, NFTCardType } from "../types/nftCardType";
-import FetchTokenIdMetadata from "../moralisAPI/fetchTokenIdMetadata";
-import { AddressContext } from "../contexts/AddressContext";
-import { NFTContext } from "../contexts/NFTContext";
+import { Grid, CardActionArea } from "@mui/material";
 import getWebMetadata from "../axios/getWebMetadata";
+import { useEffect, useState } from "react";
+interface NFTCardProps extends NFTCardDetails {}
 
-export default function ActionAreaCard() {
-  const { address, setAddress, network, setNetwork } =
-    useContext(AddressContext);
+export default function NFTCard({
+  block_number,
+  name,
+  symbol,
+  metadata: rawMetadata,
+  token_uri,
+}: NFTCardProps) {
+  const metadata = JSON.parse(rawMetadata ?? "{}");
 
-  const { nftCards, setNftCards } = useContext(NFTContext);
-
-  const [nftMetaImage, setNftMetaImage] = useState();
-
+  const [img, setImg] = useState<string>(metadata.image);
   useEffect(() => {
-    // FetchTokenIdMetadata("0xed5af388653567af2f388e6224dc7c4b3241c544", "1833");
-  }, []);
+    if (!metadata.image && token_uri && rawMetadata) {
+      getWebMetadata(token_uri).then((img: any) => setImg(img));
+    }
+  }, [token_uri, metadata.image, rawMetadata]);
 
   return (
-    <Grid container spacing={4}>
-      {nftCards?.map(({ block_number, name, symbol, metadata, token_uri }) => (
-        <Grid key={block_number} item xs={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="400"
-                image={
-                  metadata
-                    ? JSON.parse(metadata).image
-                    : token_uri && getWebMetadata(token_uri)
-                }
-                alt={metadata && JSON.stringify(JSON.parse(metadata).image)}
-                placeholder="blur"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {name ?? (metadata && JSON.parse(metadata).name)} (
-                  {symbol && symbol})
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {metadata && JSON.stringify(JSON.parse(metadata).description)}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="400"
+          image={img}
+          alt={metadata.image}
+          placeholder="blur"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name ?? metadata.name} ({symbol && symbol})
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {metadata.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
