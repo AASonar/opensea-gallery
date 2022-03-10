8138098c24bd92ede7a6ee8c3d289bc991339d64
AASonar <bryan@acadarena.com>
2022-03-10 13:15:46 +0800

    test

--- components/dashboard/queries/selectUserWalletByID.ts
+++ components/dashboard/queries/selectUserWalletByID.ts
@@ -1,25 +1,35 @@
 import { useFilter, useSelect } from "react-supabase";
 import { createClient } from "@supabase/supabase-js";
 import { useState } from "react";
+import { supabaseClient } from "../supabase/supabaseDetails";
 
-const SelectUserWalletByID = (UserID: number) => {
-  const filter = useFilter((query) =>
-    query
-      .select(
-        `
-      user_id,
-      wallet (
-        *
-      )
-    `
-      )
-      .eq("user_id", UserID)
-  );
-  const [{ count, data, error, fetching }, reexecute] = useSelect("user", {
-    filter,
-  });
+const SelectUserWalletByID = async (user_id: number) => {
+  let { data: user, error } = await supabaseClient.from("user").select(`
+    user_id,
+    wallet (
+      *
+    )
+  `);
 
-  return data && data[0].wallet;
+  return user && user[0].wallet;
+
+  // const filter = useFilter((query) =>
+  //   query
+  //     .select(
+  //       `
+  //     user_id,
+  //     wallet (
+  //       *
+  //     )
+  //   `
+  //     )
+  //     .eq("user_id", user_id)
+  // );
+  // const [{ count, data, error, fetching }, reexecute] = useSelect("user", {
+  //   filter,
+  // });
+
+  // return data && data[0].wallet;
 };
 
 export default SelectUserWalletByID;
