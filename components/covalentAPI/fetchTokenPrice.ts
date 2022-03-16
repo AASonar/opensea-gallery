60d54a6ea69e62cf1df3539f536e4843176efacf
AASonar <bryan@acadarena.com>
2022-03-16 19:28:12 +0800

    added username in title

---
+++ components/covalentAPI/fetchTokenPrice.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const FetchTokenPrice = async (
+  chain_id: string,
+  address: string,
+  currency: string = "USD"
+) => {
+  const corsUrl = "https://noki-cors.herokuapp.com";
+
+  const apiUrl = `https://api.covalenthq.com/v1/${chain_id}/address/${address}/balances_v2`;
+  const query = `?quote-currency=${currency}&format=JSON&nft=true&no-nft-fetch=false&key=${process.env.COVALENT_API_KEY}`;
+  const finalUrl = `${apiUrl}/${query}`;
+
+  const response = await axios.get(finalUrl, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const { data, status } = response;
+  const { data: data_inside } = data;
+
+  if (status === 200) {
+    //console.log(data_inside);
+    return data_inside;
+  }
+
+  return Promise.reject(response);
+};
+
+export default FetchTokenPrice;
