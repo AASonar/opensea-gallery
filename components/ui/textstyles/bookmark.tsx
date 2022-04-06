df60f184c2f3d17b9a253bcfcddee32e410ad28d
AASonar <bryan@acadarena.com>
2022-04-06 12:57:13 +0800

    added changing bookmark

--- components/ui/textstyles/bookmark.tsx
+++ components/ui/textstyles/bookmark.tsx
@@ -1,13 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ActionIcon, Space } from "@mantine/core";
+import { useToggle } from "@mantine/hooks";
 
 export default function Bookmark() {
+  const [value, toggle] = useToggle("unsaved", ["unsaved", "saved"]);
+
   return (
-    <ActionIcon
-      variant="transparent"
-      className="opacity-80 text-2xl  text-white"
-    >
-      <FontAwesomeIcon icon={["far", "bookmark"]} />
+    <ActionIcon onClick={() => toggle()} variant="transparent">
+      {value === "unsaved" ? (
+        <FontAwesomeIcon
+          icon={["far", "bookmark"]}
+          className="opacity-80 text-2xl text-white"
+        />
+      ) : (
+        <FontAwesomeIcon
+          icon={["fas", "bookmark"]}
+          className=" text-2xl text-teal"
+        />
+      )}
     </ActionIcon>
   );
 }
